1f2ce90f520b3b4c543b00fb3a9c36617108cca7
gaberielms <gabeof01@gmail.com>
2024-12-14 01:06:52 -0500

    refactor

--- app/main.c
+++ app/main.c
@@ -144,6 +144,79 @@ void build_args(char *args, arg *head) {
   }
 }
 
+void type(arg *head) {
+  if (head->next == NULL) {
+    printf("type: too few arguments\n");
+    free_args(head);
+    return;
+  }
+  char *curarg = head->next->argstr;
+  char *command = find_command(curarg);
+  if (strcmp(curarg, "cd") == 0) {
+    printf("cd is a shell builtin\n");
+  } else if (strcmp(curarg, "pwd") == 0) {
+    printf("pwd is a shell builtin\n");
+  } else if (strcmp(curarg, "echo") == 0) {
+    printf("echo is a shell builtin\n");
+  } else if (strcmp(curarg, "type") == 0) {
+    printf("type is a shell builtin\n");
+  } else if (strcmp(curarg, "exit") == 0) {
+    printf("exit is a shell builtin\n");
+  } else if (command != NULL) {
+    printf("%s is %s\n", curarg, command);
+    free(command);
+  } else {
+    printf("%s: not found\n", curarg);
+  }
+}
+
+void change_directory(arg *head) {
+  if (head->next == NULL) {
+    char *home = getenv("HOME");
+    if (chdir(home) != 0) {
+      printf("cd: %s: No such file or directory\n", home);
+    }
+  }
+  else if (strchr(head->next->argstr, '~') != NULL) {
+    char *home = getenv("HOME");
+    char *new_arg = malloc(strlen(home) + strlen(head->argstr) + 1);
+    if (new_arg == NULL) {
+      fprintf(stderr, "Failed to allocate memory for new_arg\n");
+      free_args(head);
+      exit(1);
+    }
+    strcpy(new_arg, home);
+    strcat(new_arg, head->next->argstr + 1);
+    if (chdir(new_arg) != 0) {
+      printf("cd: %s: No such file or directory\n", new_arg);
+    }
+    free(new_arg);
+  } else if (chdir(head->next->argstr) != 0) {
+    printf("cd: %s: No such file or directory\n", head->next->argstr);
+  }
+}
+
+void pwd() {
+  char cwd[100];
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    printf("%s\n", cwd);
+  } else {
+    printf("Failed to get current working directory\n");
+  }
+}
+
+void echo(arg *head) {
+  arg *current = head->next;
+  while (current != NULL) {
+    printf("%s", current->argstr);
+    current = current->next;
+    if (current != NULL) {
+      printf(" ");
+    }
+  }
+  printf("\n");
+}
+
 int main() {
   while (1) {
     printf("$ ");
@@ -158,85 +231,28 @@ int main() {
     // Build arg linked list
     arg *head = build_arg(NULL);
     build_args(args, head);
-    arg *current = head;
     // BUILTIN COMMANDS
     if (strcmp(head->argstr, "exit") == 0) {
       free_args(head);
       exit(0);
     } else if (strcmp(head->argstr, "cd") == 0) {
-      if (head->next == NULL) {
-        char *home = getenv("HOME");
-        if (chdir(home) != 0) {
-          printf("cd: %s: No such file or directory\n", home);
-        }
-      }
-      else if (strchr(head->next->argstr, '~') != NULL) {
-        char *home = getenv("HOME");
-        char *new_arg = malloc(strlen(home) + strlen(head->argstr) + 1);
-        if (new_arg == NULL) {
-          fprintf(stderr, "Failed to allocate memory for new_arg\n");
-          free_args(head);
-          exit(1);
-        }
-        strcpy(new_arg, home);
-        strcat(new_arg, head->next->argstr + 1);
-        if (chdir(new_arg) != 0) {
-          printf("cd: %s: No such file or directory\n", new_arg);
-        }
-        free(new_arg);
-      } else if (chdir(head->next->argstr) != 0) {
-        printf("cd: %s: No such file or directory\n", head->next->argstr);
-      }
+      change_directory(head);
     } else if (strcmp(head->argstr, "echo") == 0) {
-      current = head->next;
-      while (current != NULL) {
-        printf("%s", current->argstr);
-        current = current->next;
-        if (current != NULL) {
-          printf(" ");
-        }
-      }
-      printf("\n");
+      echo(head);
     } else if (strcmp(head->argstr, "pwd") == 0) {
-      char cwd[100];
-      if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("%s\n", cwd);
-      } else {
-        printf("Failed to get current working directory\n");
-      }
+      pwd();
     } else if (strcmp(head->argstr, "type") == 0) {
-      if (head->next == NULL) {
-        printf("type: too few arguments\n");
-        free_args(head);
-        continue;
-      }
-      char *curarg = head->next->argstr;
-      char *command = find_command(curarg);
-      if (strcmp(curarg, "cd") == 0) {
-        printf("cd is a shell builtin\n");
-      } else if (strcmp(curarg, "pwd") == 0) {
-        printf("pwd is a shell builtin\n");
-      } else if (strcmp(curarg, "echo") == 0) {
-        printf("echo is a shell builtin\n");
-      } else if (strcmp(curarg, "type") == 0) {
-        printf("type is a shell builtin\n");
-      } else if (strcmp(curarg, "exit") == 0) {
-        printf("exit is a shell builtin\n");
-      } else if (command != NULL) {
-        printf("%s is %s\n", curarg, command);
-        free(command);
-      } else {
-        printf("%s: not found\n", curarg);
-      }
+      type(head);
+    }
     // EXTERNAL COMMANDS
-    } else if (input[0] != '\0') {
+    else if (input[0] != '\0') {
       // Fork and exec external command
       char *command = find_command(head->argstr);
       if (command != NULL) {
         pid_t pid = fork();
         if (pid == 0) {
           int argc = 0;
-          current = head->next;
+          arg *current = head->next;
           while (current != NULL) {
             argc++;
             current = current->next;
