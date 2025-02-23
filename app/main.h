71959f96a144453aafcaef104d36473efeb2bb16
gabe <gabeof01@gmail.com>
2025-02-23 16:28:39 -0500

    add in memory history and fix arrow keys

--- app/main.h
+++ app/main.h
@@ -3,13 +3,19 @@
 
 #define MAX_INPUT_SIZE 1024 // Maximum input size
 #define MAX_USERNAME_SIZE 32 // Maximum username size
+#define MAX_HISTORY 10 // Maximum history size
 
 #include <unistd.h>
+#include <termios.h>
 #include "types.h"
 #include "commands.h"
 #include "parser.h"
 #include "executor.h"
 #include "utils.h"
+
+void disable_raw_mode(); // Disable raw mode
+
+void enable_raw_mode(); // Enable raw mode
 
 int main(); // Main function
 
